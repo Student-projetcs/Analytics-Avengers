d31aaad6c28198b6850d950c37e17eb2bc2223ef
by-ST <129612472+by-ST@users.noreply.github.com>
2023-07-27 12:22:49 +0100

    Update app-python.py
    Not yet finished

--- app/app-python.py
+++ app/app-python.py
@@ -5,17 +5,39 @@ import dash
 from dash import Dash, html, dcc
 
 #Dataset importation 1
+#Importing Child Mortality file
+df = pd. read_csv('C:/Users/SonyY/Documents/Portfolio Project/Infant Mortality Rate.csv')
+
 
 #Dataset importation 2
 
 #Dataset importation 3
 
 #Figure1
+fig_map = px.choropleth(df_latest_btsx, locations='CountryCode', color='MortalityRate', hover_name='Country',
+                    projection='natural earth',
+                    title='Child Mortality Rate by Country (Both Sex, Latest Year)',
+                    labels={'MortalityRate': 'Mortality Rate',
+                            'Sex' : 'Sex'
+                        })
+
 
 #figure 2
-
+fig_years_region = px.line(
+    df_years_sex_region, x='Year', y='MortalityRate',
+    color='Region', line_group='Sex',  
+    line_dash='Sex',  
+    markers=True,  
+    title='Child Mortality Rate (Under 5) by Region and Sex',
+    labels={'Year': 'Year', 'MortalityRate': 'Mortality Rate'},
 #figure 3
+fig_avg_mortality = px.bar(
+                    df_avg_mortality, x = 'Average_Percentage_Change', y = 'Region', color = 'Sex', orientation='h',
+                    barmode = 'group', title = 'Avg. Child Mortality Rate Change (2002-2021) by Region'
+                    
+)
 
+fig_avg_mortality.update_xaxes(title_text="Average Mortality Rate Percentage Change (%)")
 
 # Initialize the app
 app = Dash(__name__)
